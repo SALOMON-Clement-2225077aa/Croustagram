4b8de9106e4cbff033c84ccf0d794bd95912f744
NathanGOUACHEG22017449 <123471299+NathanGOUACHEG22017449@users.noreply.github.com>
2023-09-07 08:51:57 +0000

    Changement pour du php

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
-<!DOCTYPE html>
-<html lang='fr'>
-<head>
-    <meta charset="UTF-8">
-    <meta name="titre" content="Page d'accueil">
-    <meta name="description" content="Page principale sur laquelle l'utilisateur est censé tomber en premier">
-    <title>Croustagram - Accueil</title>
-</head>
+<?php
+    function start_page($title): void
+    {
 
-<body>
-    <h1>Hello World !</h1>
-    <h1>Ceci est la première version de Croustagram !</h1>
-</body>
+        ?><!DOCTYPE html>
+        <html lang='fr'>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="titre" content="Page d'accueil">
+        <meta name="description" content="Page principale sur laquelle l'utilisateur est censé tomber en premier">
+        <title><?php echo $title; ?></title>
+    </head>
+
+    <body>
+        <h1>Hello World !</h1>
+        <h1>Ceci est la première version de Croustagram !</h1>
+    </body>
+<?php
+    }
+?>
+
+<?php
+    start_page('Croustagram - Accueil');
+?>
