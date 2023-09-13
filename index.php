0c8884049ae365c82498089a4b62fafc9a6992f0
Clément Salomon <114286471+ClementSalomonS22005748@users.noreply.github.com>
2023-09-13 18:46:45 +0200

    Import de 'utils.inc' + suppression des fonctions

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
-<?php
-    function start_page($title): void
-    {
+<!-- Import des fonctions -->
+<?php require 'utils.inc.php'; ?>
 
-        ?><!DOCTYPE html>
-        <html lang='fr'>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="titre" content="Page d'accueil">
-        <meta name="description" content="Page principale sur laquelle l'utilisateur est censé tomber en premier">
-        <title><?php echo $title; ?></title>
-    </head>
 
-    <body>
-        <h1>Hello World !</h1>
-        <h1>Ceci est la première version de Croustagram !</h1>
-    </body>
-<?php
-    }
-?>
-
+<!-- Contenu de la page -->
 <?php
     start_page('Croustagram - Accueil');
+
+    echo '<br><br>';
+
+    end_page();
 ?>
