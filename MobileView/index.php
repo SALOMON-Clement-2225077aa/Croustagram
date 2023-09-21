1995c0e3dfee7d42243660751b22aad26a19ea6b
GruZbob <alexandre.bergeron@etu.univ-amu.fr>
2023-09-21 22:34:03 +0200

    Mobile Home Page

--- MobileView/index.php
+++ MobileView/index.php
@@ -1,7 +1,12 @@
 <?php
 
-require 'utils.inc.php';
+require './MobileView/utils.inc.php';
 
-start_page('Thèmes');
+start_page('Fil d\'actualité');
+
+echo '<br><br><br><br><br><br><br><br>a<br>b<br>c<br>d<br>e<br>f<br>g<br>h<br>i<br>j<br>k<br>l<br>m<br>n<br>o<br>p<br>q<br>r<br>s<br>t<br>u<br>v<br>w<br>x<br>y<br>z<br>
+a<br>b<br>c<br>d<br>e<br>f<br>g<br>h<br>i<br>j<br>k<br>l<br>m<br>n<br>o<br>p<br>q<br>r<br>s<br>t<br>u<br>v<br>w<br>x<br>y<br>z<br><br><br><br><br>';
+
+end_page('Fil d\'actualité');
 
 ?>
--- MobileView/utils.inc.php
+++ MobileView/utils.inc.php
@@ -14,20 +14,35 @@ function start_page($title) :void
         <meta name="titre" content="Page d'accueil">
         <meta name="description" content="Page principale sur laquelle l'utilisateur est censé tomber en premier">
         <link rel="stylesheet" href="style.css">
-        <title><?php echo 'Croustagram'; ?></title>
+        <title>Croustagram</title>
     </head>
     <body>
         <header>
-            <img id="Logo" src="../ressources/logo.png">
-            <div id="DivBarreRecherche">
-                <button id="Recherche" onclick=""></button>
-                <input id="BarreRecherche" type="text">
-                <button id="EffacerRecherche" onclick=""></button>
-                <button id="FiltrerRecherche" onclick=""></button>
+            <div id="DivLogoBarre">
+                <img id="Logo" src="../ressources/logo.png">
+                <div id="DivBarreRecherche">
+                    <button id="Recherche" onclick=""></button>
+                    <input id="BarreRecherche" type="text">
+                    <button id="EffacerRecherche" onclick=""></button>
+                    <button id="FiltrerRecherche" onclick=""></button>
+                </div>
             </div>
+            <h1><?php echo $title ?></h1>
         </header>
-    </body>
     <?php
 }
 
+function end_page($title): void
+{
+    ?>
+    <!DOCTYPE html>
+    <html lang="fr">
+        <footer>
+            <button id="BoutonHome"></button>
+            <button id="BoutonLeaderboard"></button>
+            <button id="BoutonProfil"></button>
+        </footer>
+    </body>
+    <?php
+}
 ?>
