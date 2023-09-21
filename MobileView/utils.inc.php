c11246136f368cccf078c9918f67b7f9be3a7db3
Alexandre BERGERON <alexandrebergeron12@gmail.com>
2023-09-21 09:20:59 +0200

    Création de start page pour mobile

---
+++ MobileView/utils.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Fonction start_page
+ */
+
+function start_page($title) :void
+{
+    ?>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="titre" content="Page d'accueil">
+        <meta name="description" content="Page principale sur laquelle l'utilisateur est censé tomber en premier">
+        <title><?php echo Croustagram; ?></title>
+    </head>
+}
+
+?>
