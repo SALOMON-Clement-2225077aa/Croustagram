fc3f8ced769c6467e18cdb46fd2ac41c62e0ea5a
NathanGOUACHEG22017449 <123471299+NathanGOUACHEG22017449@users.noreply.github.com>
2023-09-22 19:05:26 +0200

    Update index.php
    test insertion creation compte dans le mobileview

--- MobileView/ProfilPage/index.php
+++ MobileView/ProfilPage/index.php
@@ -6,10 +6,68 @@ start_page('Profil');
 
 ?>
 <div id="ContenuPage">
-    <?php
-    echo 'a<br>b<br>c<br>d<br>e<br>f<br>g<br>h<br>i<br>j<br>k<br>l<br>m<br>n<br>o<br>p<br>q<br>r<br>s<br>t<br>u<br>v<br>w<br>x<br>y<br>z<br>
-    a<br>b<br>c<br>d<br>e<br>f<br>g<br>h<br>i<br>j<br>k<br>l<br>m<br>n<br>o<br>p<br>q<br>r<br>s<br>t<br>u<br>v<br>w<br>x<br>y<br>z';
-    ?>
+    <!-- Page de création du compte -->
+<?php
+function account_page($erreurTab = array(), $def_username = NULL, $def_mail = NULL, $def_name = NULL): void
+{
+
+?>
+<body>
+    <h1>Prêt à vivre l'expérience Croustagram ?</h1>
+    <form action="/creationCompte/createAccount.php" method="post">
+
+        <label>E-Mail :</label><br><br>
+        <input type='text' name='mail' required value=<?php echo '\'' . $def_mail . '\''; ?><br><br>
+        <?php
+            if (in_array("mail", $erreurTab))
+        {
+            echo '<strong style=\'color:red;\'>Format d\'adresse e-mail invalide</strong>';
+        }
+            elseif (in_array("mailPris", $erreurTab))
+        {
+            echo '<strong style=\'color:red;\'>Adresse e-mail déjà utilisée par un autre compte</strong>';
+        }
+        ?>
+
+        <br><label>Nom d'utilisateur :</label><br><br>
+        <input type='text' name='username' required value=<?php echo '\'' . $def_username . '\''; ?><br><br>
+        <?php
+            if (in_array("username", $erreurTab))
+            {
+                echo '<strong style=\'color:red;\'>Le nom d\'utilisateur ne peut contenir que des caractères alphanumériques</strong>';
+            }
+            elseif (in_array("usernamePris", $erreurTab))
+            {
+                echo '<strong style=\'color:red;\'>Ce nom d\'utilisateur est déjà utilisé</strong>';
+            }
+        ?>
+
+        <br><label>Mot de passe :</label><br><br>
+        <input type='password' name='password' required><br>
+        <?php
+            if (in_array("password", $erreurTab)){
+                echo '<strong style=\'color:red;\'>Le mot de passe doit faire minimum 8 caractères</strong>';
+            }
+        ?>
+        <br><label>Vérification du mot de passe :</label><br><br>
+        <input type='password' name='passwordMatch' required><br>
+        <?php
+        if (in_array("passwordMatch", $erreurTab)){
+            echo '<strong style=\'color:red;\'>Les mots de passe ne correspondent pas !</strong>';
+        }
+        ?>
+
+
+        <br><label>Nom d'affichage </label><label style="color: red">(optionnel)</label><label> :</label><br><br>
+        <input type='text' name='name' value=<?php echo '\'' . $def_name . '\''; ?>><br>
+        <br><button type="submit" value="mailer" name="action">Rejoindre Croustagram !</button>
+
+    </form>
+</body>
+<?php
+}
+?>
+<!--------------------------------->
 </div>
 
 <?php
