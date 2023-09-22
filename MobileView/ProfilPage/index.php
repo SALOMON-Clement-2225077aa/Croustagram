3f05cf8d830a03b3129a3e3c53a602952eb09fe0
NathanGOUACHEG22017449 <nathan.gouache@etu.univ-amu.fr>
2023-09-22 22:59:31 +0200

    Changement au lieu de mettre la création dans la page profil on en fait une autre fenêtre

--- MobileView/ProfilPage/index.php
+++ MobileView/ProfilPage/index.php
@@ -1,15 +1,12 @@
 <?php
 
 require '../utils.inc.php';
-require '../../creationCompte/utils.createaccount.php';
 
 start_page('Profil');
 
 ?>
     <div id="ContenuPage">
-        <?php
-            account_page();
-        ?>
+        //  Vide
     </div>
 
 <?php
