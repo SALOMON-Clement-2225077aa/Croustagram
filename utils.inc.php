bb03e482b8cdf7723b4bc0f00f3727daf2cf9c87
Clément Salomon <114286471+ClementSalomonS22005748@users.noreply.github.com>
2023-09-24 01:18:31 +0200

    Implémentation de afficher_post() pour téléphone

--- utils.inc.php
+++ utils.inc.php
@@ -1,69 +1,52 @@
-<!-- Fonction start_page('titre') -->
 <?php
-    function start_page($title): void
-    {
 
-        ?><!DOCTYPE html>
-        <html lang='fr'>
+/*
+ * Fonction start_page
+ */
+
+function start_page($title) :void
+{
+    ?>
+    <!DOCTYPE html>
+    <html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta name="titre" content="Page d'accueil">
         <link rel="icon" href="/ressources/logo.png" />
-        <meta name="description" content="Page d'accueil de Croustagram - Dekstop">
+        <meta name="description" content="Page d'accueil de Croustagram - Mobile">
         <link rel="stylesheet" href="style.css">
-        <title><?php echo $title; ?></title>
+        <title>Croustagram</title>
     </head>
-
     <body>
-    <header>
-        <img class="header" id="logo" src="/recources/1349px-Logo_Crous_vectorisé.svg.png">
-        <h1 class="header">Croustagram</h1>
-        <button onclick="ouvrirPost()"> Créer un croustapost </button>
-        <button onclick="window.location.href = 'creationCompte/pageCreationCompte.php';" style="margin-left: 10px"> Rejoindre la croustagrammance </button>
-        <button onclick="window.location.href = 'connexionCompte/pageConnexionCompte.php';" style="margin-left: 10px"> Se connecter à un compte </button>
-        <button onclick="window.location.href = 'MobileView/HomePage/index.php';" style="margin-left: 10px"> Accéder à la version mobile </button>
-    </header>
+        <header>
+            <div id="DivLogoBarre">
+                <img id="Logo" src="../../ressources/logo.png">
+                <div id="DivBarreRecherche">
+                    <button id="Recherche" onclick=""></button>
+                    <input id="BarreRecherche" type="text">
+                    <button id="EffacerRecherche" onclick=""></button>
+                    <button id="FiltrerRecherche" onclick=""></button>
+                </div>
+            </div>
+            <h1><?php echo $title ?></h1>
+        </header>
+    <?php
+}
 
-    <section id="leaderboard">
-        <h2>Leaderboard :</h2>
-    </section>
-
-    <div id="body">
-        <section id="posts">
-            <article class="post">
-                <h2>aucun post</h2>
-            </article>
-        </section>
-
-    <section id="pointCpt">
-        <h2>Mes points crous : 0</h2>
-    </section>
-
-    <section id="ad">
-        <h3>your ad here</h3>
-    </section>
-
-    </body>
-<?php
-    }
-?>
-<!--------------------------------->
-
-<!-- Fonction end_page -->
-<?php
-    function end_page(): void
-    {
-?>
+function end_page($title): void
+{
+    ?>
     <!DOCTYPE html>
-    <html lang='fr'>
-    <body>
+    <html lang="fr">
+        <footer>
+            <button id="BoutonHome" onclick="window.location.href='../HomePage/index.php';"></button>
+            <button id="BoutonLeaderboard" onclick="window.location.href='../LeaderboardPage/index.php';"></button>
+            <button id="BoutonProfil" onclick="window.location.href='../ProfilPage/index.php';"></button>
+        </footer>
     </body>
-<?php 
-    }
+    <?php
+}
 ?>
-<!---------------------->
-
-
 
 <!-- Afficher un post --> 
 <?php
@@ -74,7 +57,7 @@
 <div id="post">
     <table id="tabPost">
         <tr>
-            <th><img src="ressources/profil.png" id="imgProfil"> <?php echo $croustagrameur ?> </th>
+            <th><img src="../../ressources/profil.png" id="imgProfil"> <?php echo $croustagrameur ?> </th>
             <th id="titrePost"><?php
                 echo '<h1>' . $titre . '</h1>';
                 ?></th>
@@ -89,11 +72,11 @@
         </tr>
         <tr>
             <th> <?php echo $ptsCrous ?>
-                <img src="ressources/fleche-vers-le-haut.png" id="imgProfil">
-                <img src="ressources/fleche-vers-le-bas.png" id="imgProfil">
+                <img src="../../ressources/fleche-vers-le-haut.png" id="imgProfil">
+                <img src="../../ressources/fleche-vers-le-bas.png" id="imgProfil">
             </th>
             <th> <?php echo $categorie ?> </th>
-            <th><img src="ressources/commentaire.png" id="imgProfil"></th>
+            <th><img src="../../ressources/commentaire.png" id="imgProfil"></th>
         </tr>
     </table>
 </div>
