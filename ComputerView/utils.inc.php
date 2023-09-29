ba07324bbb7ec4ca3fcbe7597f036471ea1a1a03
clement <clement.salomon@etu.univ-amu.fr>
2023-09-29 12:03:43 +0200

    Ajout du classement sur la version PC

--- ComputerView/utils.inc.php
+++ ComputerView/utils.inc.php
@@ -48,6 +48,30 @@
 
     <section id="leaderboard">
         <h2>Leaderboard :</h2>
+        <?php // Lecture + Affichage des utilisateurs de la BD (SELECT * FROM `croustagrameur`)
+
+        // Connexion à la base de donnée
+        $dbLink = mysqli_connect("mysql-croustagramadd.alwaysdata.net", 328031, "b1Gz0000")
+        or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+        mysqli_select_db($dbLink , "croustagramadd_bdd")
+        or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+
+        // Requête
+        $result = mysqli_query($dbLink, 'SELECT * FROM croustagrameur ORDER BY ptsCrous DESC');
+
+        // Si la requête a marché on affiche les users
+        if ($result) {
+            // afficher_user($pseudo, $img, $date_creation, $date_connexion, $ptsCrous):
+            while ($row = mysqli_fetch_assoc($result)) {
+                afficher_user($row['pseudo'], $row['img'], $row['creation_compte'], $row['derniere_connexion'], $row['ptsCrous']);
+            }
+            // Libère la variable
+            mysqli_free_result($result);
+        }
+        else {
+            echo 'Erreur dans la requête : ' . mysqli_error($dbLink);
+        }
+        ?>
     </section>
 <?php
     }
@@ -109,6 +133,21 @@
 </form>
 <?php
 }
+}
+?>
+<!---------------------->
+
+<!-- Afficher un user -->
+<?php
+function afficher_user($pseudo, $img, $date_creation, $date_connexion, $ptsCrous) {?>
+    <div id="User">
+        <img src="../../ressources/profil.png" id="imgProfil">
+        <div>
+            <th><?php echo $pseudo ?></th>
+            <th><br><?php echo $ptsCrous ?></th>
+        </div>
+    </div>
+    <?php
 }
 ?>
 <!---------------------->
