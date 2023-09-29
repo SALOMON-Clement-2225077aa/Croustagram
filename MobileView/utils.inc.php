cc09c4c8cefff0d2328eba99bd557eb1b165e321
Clément Salomon <114286471+ClementSalomonS22005748@users.noreply.github.com>
2023-09-29 20:59:33 +0200

    Fix paths

--- MobileView/utils.inc.php
+++ MobileView/utils.inc.php
@@ -1,65 +1,109 @@
+<!-- Fonction start_page('titre') -->
 <?php
-
-/*
- * Fonction start_page
- */
-
-function start_page($title) :void
-{
     $isMob = is_numeric(strpos(strtolower($_SERVER["HTTP_USER_AGENT"]), "mobile"));
-    if(!$isMob){header("Location: ../../ComputerView/index.php");}
-    ?>
-    <!DOCTYPE html>
-    <html lang="fr">
+    if($isMob){header("Location: ../MobileView/HomePage/index.php");}
+    else
+    {
+    session_start();
+    function start_page($title): void
+    {
+
+        ?><!DOCTYPE html>
+        <html lang='fr'>
     <head>
         <meta charset="UTF-8">
         <meta name="titre" content="Page d'accueil">
-        <link rel="icon" href="/ressources/logo.png" />
-        <meta name="description" content="Page d'accueil de Croustagram - Mobile">
+        <link rel="icon" href="../ressources/logo.png" />
+        <meta name="description" content="Page d'accueil de Croustagram - Dekstop">
         <link rel="stylesheet" href="style.css">
-        <title><?php echo 'Croustagram - '.$title?></title>
+        <title><?php echo $title; ?></title>
     </head>
+
     <body>
-        <header>
-            <div id="DivLogoBarre">
-                <img id="Logo" src="../../ressources/logo.png">
-                <div id="DivBarreRecherche">
-                    <button id="Recherche" onclick=""></button>
-                    <input id="BarreRecherche" type="text">
-                    <button id="EffacerRecherche" onclick=""></button>
-                    <button id="FiltrerRecherche" onclick=""></button>
-                </div>
+    <header>
+        <div id="divLogo">
+            <img class="header" id="logo" src="../ressources/logo.png">
+            <h1 class="header">Croustagram</h1>
+        </div>
+        <div class="header" id="DivLogoBarre">
+            <div id="DivBarreRecherche">
+                <button id="Recherche" onclick=""></button>
+                <input id="BarreRecherche" type="text">
+                <button id="EffacerRecherche" onclick=""></button>
+                <button id="FiltrerRecherche" onclick=""></button>
             </div>
-            <h1 id="PageTitre"><?php echo $title ?></h1>
-        </header>
-    <?php
-}
+        <?php
+        if(isset($_SESSION['suid']))
+        {
+            echo '<button onclick="ouvrirPost()" style="left:850px; top:50px; position:fixed"> Créer un croustapost </button>';
+            echo '<label style="top: 40px; right: 20px; position: fixed">Connecté en tant que : ' . $_SESSION['username'] . '</label>';
+            echo '<button onclick="window.location.href = \'logout.php\';" style="right: 10px; top: 60px; position: fixed"> Se déconnecter </button>';
+        }
+        else
+        {
+            echo '<button onclick="window.location.href = \'../connexionCompte/pageConnexionCompte.php\';" style="right: 20px; top:50px; position: fixed"> Se connecter à un compte/s\'inscrire</button>';
+        }
+        ?>
+    </header>
 
-function end_page($title): void
-{
-    ?>
-    <!DOCTYPE html>
-    <html lang="fr">
-        <footer>
-            <button id="BoutonHome" onclick="window.location.href='../HomePage/index.php';"></button>
-            <button id="BoutonLeaderboard" onclick="window.location.href='../LeaderboardPage/index.php';"></button>
-            <button id="BoutonProfil" onclick="window.location.href='../ConnexionPage/index.php';"></button>
-        </footer>
-    </body>
-    <?php
-}
+    <section id="leaderboard">
+        <h2>Leaderboard :</h2>
+        <?php // Lecture + Affichage des utilisateurs de la BD (SELECT * FROM `croustagrameur`)
+
+        // Connexion à la base de donnée
+        $dbLink = mysqli_connect("mysql-croustagramadd.alwaysdata.net", 328031, "b1Gz0000")
+        or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+        mysqli_select_db($dbLink , "croustagramadd_bdd")
+        or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+
+        // Requête
+        $result = mysqli_query($dbLink, 'SELECT * FROM croustagrameur ORDER BY ptsCrous DESC');
+
+        // Si la requête a marché on affiche les users
+        if ($result) {
+            // afficher_user($pseudo, $img, $date_creation, $date_connexion, $ptsCrous):
+            while ($row = mysqli_fetch_assoc($result)) {
+                afficher_user($row['pseudo'], $row['img'], $row['creation_compte'], $row['derniere_connexion'], $row['ptsCrous']);
+            }
+            // Libère la variable
+            mysqli_free_result($result);
+        }
+        else {
+            echo 'Erreur dans la requête : ' . mysqli_error($dbLink);
+        }
+        ?>
+    </section>
+<?php
+    }
 ?>
+<!--------------------------------->
 
-<!-- Afficher un post --> 
+<!-- Fonction end_page -->
+<?php
+    function end_page(): void
+    {
+?>
+    <!DOCTYPE html>
+    <html lang='fr'>
+    <body>
+    </body>
+<?php
+    }
+?>
+<!---------------------->
+
+
+
+<!-- Afficher un post -->
 <?php
     function afficher_post($croustagrameur, $titre, $message, $date, $categorie1, $categorie2, $categorie3, $ptsCrous, $idPost): void
     {
 ?>
-<br><br><br><br>
-<div id="post">
+<form action="../managePost/pagePost.php" >
+<div id="post" style="margin-bottom: 25px">
     <table id="tabPost">
         <tr>
-            <th><img src="../../ressources/profil.png" id="imgProfil"> <?php echo $croustagrameur ?> </th>
+            <th><img src="../ressources/profil.png" id="imgProfil" ></th>
             <th id="titrePost"><?php
                 echo '<h1>' . $titre . '</h1>';
                 ?></th>
@@ -69,42 +113,41 @@ function end_page($title): void
         </tr>
         <tr>
             <th colspan="3"><?php
-
-                for ($i=0; $i<strlen($message); $i=$i+30) {
-                    ?>
-                    <h2> <?php echo substr($message, $i, $i+30); ?> </h2><br> <?php
-                }
-                ?>
-
-                </th>
+                echo '<h2>' . $message . '</h2>';
+                ?></th>
         </tr>
         <tr>
             <th> <?php echo $ptsCrous ?>
-                <button onclick="upVote()" id="UpVoteBouton">
-                <button onclick="downVote()" id="DownVoteBouton">
+                <button onclick="upVote()"> <img src="../ressources/fleche-vers-le-haut.png" id="imgProfil"> </button>
+                <button onclick="downVote()"> <img src="../ressources/fleche-vers-le-bas.png" id="imgProfil"> </button>
             </th>
             <th> <?php echo $categorie1 . ', ' ; echo $categorie2 . ', ' ; echo $categorie3 ?> </th>
-            <th><button id="CommentaireBouton"></th>
+            <th>
+                <a href="../managePost/pagePost.php?id=<?php echo $idPost?>">
+                    <img src="../ressources/commentaire.png" id="imgProfil">
+                </a>
+            </th>
         </tr>
     </table>
 </div>
+</form>
 <?php
+}
 }
 ?>
 <!---------------------->
-
 
 <!-- Afficher un user -->
 <?php
 function afficher_user($pseudo, $img, $date_creation, $date_connexion, $ptsCrous) {?>
     <div id="User">
-        <img src="../../ressources/profil.png" id="imgProfil">
+        <img src="../ressources/profil.png" id="imgProfil">
         <div>
             <th><?php echo $pseudo ?></th>
             <th><br><?php echo $ptsCrous ?></th>
         </div>
     </div>
-<?php
+    <?php
 }
 ?>
 <!---------------------->
