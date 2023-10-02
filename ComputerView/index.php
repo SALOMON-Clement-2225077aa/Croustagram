85d7367447ec6cdc3394f58125cf67a3bc9d9fc2
LeoG <103144119+LeoG1904@users.noreply.github.com>
2023-10-02 10:04:57 +0200

    barre recherche back

--- ComputerView/index.php
+++ ComputerView/index.php
@@ -32,7 +32,7 @@
     or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
 
     // Requête
-    $recherche = recherche_post("Coucou");
+    $recherche = 'SELECT * FROM croustapost ORDER BY ptsCrous DESC';
     $result = mysqli_query($dbLink, $recherche);
 
     // Si la requête a marché on affiche les posts
