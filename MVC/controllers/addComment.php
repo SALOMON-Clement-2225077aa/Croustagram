e9d1264c936dbca40938b87cb2a361723b56e22a
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2023-10-11 11:59:40 +0200

    Fix des posts

--- MVC/controllers/addComment.php
+++ MVC/controllers/addComment.php
@@ -7,7 +7,7 @@ require_once '../config/connectDatabase.php';
 
     $connexion = connexion();
 
-    $query = 'INSERT INTO croustacomm (texte, date, croustagrameur_id, croustapost_id, pts_crous) VALUES ("' .  $commentContent . '", "' . $today . '", "' . $_SESSION['username'] . '", "' . $_GET['id'] . '")';
+    $query = 'INSERT INTO croustacomm (texte, date, croustagrameur_id, croustapost_id) VALUES ("' .  $commentContent . '", "' . $today . '", "' . $_SESSION['username'] . '", "' . $_GET['id'] . '")';
 
     if (!($dbResult = $connexion->exec($query))) {
         echo '<strong>Erreur dans requête</strong><br>';
