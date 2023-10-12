670c99f80fc3110e7a9ed91a18ff6960e34abfe0
NathanGOUACHEG22017449 <nathan.gouache@etu.univ-amu.fr>
2023-10-12 14:21:38 +0200

    Fix erreur redirection après suppression d'un post

--- MVC/models/deleteCommsAndPosts.php
+++ MVC/models/deleteCommsAndPosts.php
@@ -9,14 +9,16 @@ if($_GET['commId'] != null){
     $verifData = $connexion->query('SELECT croustagrameur_id FROM croustacomm WHERE id=' . $_GET['commId']);
     while($verif = $verifData->fetch(PDO::FETCH_ASSOC)){
         if ($verif['croustagrameur_id'] === $_SESSION['username']) $connexion->exec('DELETE FROM croustacomm WHERE id=' . $_GET['commId']);
+        header("Location: ../views/viewPoste.php?id=" . $_GET['postId']);
+        exit();
     }
 }
 else {
     $verifData = $connexion->query('SELECT croustagrameur_id FROM croustapost WHERE id=' . $_GET['postId']);
     while ($verif = $verifData->fetch(PDO::FETCH_ASSOC)){
         if ($verif['croustagrameur_id'] === $_SESSION['username']) $connexion->exec('DELETE FROM croustapost WHERE id=' . $_GET['postId']);
+        header("Location: ../views/viewMainPage.php");
+        exit();
     }
 }
 
-header("Location: ../views/viewPoste.php?id=" . $_GET['postId']);
-exit();
