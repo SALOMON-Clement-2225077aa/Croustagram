7e597ba7aa67e99bddf3361ff7a858874fe4f981
Clément Salomon <114286471+ClementRKG@users.noreply.github.com>
2023-10-13 01:07:47 +0200

    Votes : n'update plus les ptsCrous du post mais ajoute le tuple ('id_user','id_post') à la table vote

--- MVC/models/modelVote.php
+++ MVC/models/modelVote.php
@@ -8,7 +8,9 @@ function upVote($post_ID) : void
     global $connexion;
 
     // Requête
-    $requete = 'UPDATE croustapost SET ptsCrous = ptsCrous + 1 WHERE id = ' . $post_ID;
+    $requete = 'INSERT INTO croustavote (croustagrameur_id, croustapost_id, up_vote, down_vote) VALUES (' . '\'' . $_SESSION['username']. '\', ' . $post_ID . ', '  . 1 . ', ' . 0 . ')';
+    echo $requete;
+
     $connexion->exec($requete);
 
     session_start();
@@ -19,7 +21,8 @@ function downVote($post_ID) : void
     global $connexion;
 
     // Requête
-    $requete = 'UPDATE croustapost SET ptsCrous = ptsCrous - 1 WHERE id = ' . $post_ID;
+    $requete = 'INSERT INTO croustavote (croustagrameur_id, croustapost_id, up_vote, down_vote) VALUES (' . '\'' . $_SESSION['username']. '\', ' . $post_ID . ', '  . 0 . ', ' . 1 . ')';
+
     $connexion->exec($requete);
 
     session_start();
