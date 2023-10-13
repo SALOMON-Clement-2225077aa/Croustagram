648aa82f94f77d463a092bc13b2acdaa6eaae0d8
Clément Salomon <114286471+ClementRKG@users.noreply.github.com>
2023-10-13 11:56:12 +0200

    Autorisation à 1 vote unique max

--- MVC/models/modelVote.php
+++ MVC/models/modelVote.php
@@ -8,10 +8,12 @@ function upVote($post_ID) : void
     global $connexion;
 
     // Requête
-    $requete = 'INSERT INTO croustavote (croustagrameur_id, croustapost_id, up_vote, down_vote) VALUES (' . '\'' . $_SESSION['username']. '\', ' . $post_ID . ', '  . 1 . ', ' . 0 . ')';
-    echo $requete;
+    $requete = 'INSERT INTO croustavote (croustagrameur_id, croustapost_id, up_vote, down_vote) VALUES (' . '\'' . $_SESSION['username'] . '\', ' . $post_ID . ', '  . 1 . ', ' . 0 . ')';
 
-    $connexion->exec($requete);
+    if(dejaVote($_SESSION['username'],$post_ID) == 0) {
+        $connexion->exec($requete);
+        $connexion->exec('UPDATE croustapost SET ptsCrous = ptsCrous + 1 WHERE id = ' . $post_ID);
+    }
 
     session_start();
     header('Location: ' . $_SESSION['currentUrl']);
@@ -23,8 +25,24 @@ function downVote($post_ID) : void
     // Requête
     $requete = 'INSERT INTO croustavote (croustagrameur_id, croustapost_id, up_vote, down_vote) VALUES (' . '\'' . $_SESSION['username']. '\', ' . $post_ID . ', '  . 0 . ', ' . 1 . ')';
 
-    $connexion->exec($requete);
+    if(dejaVote($_SESSION['username'],$post_ID) == 0) {
+        $connexion->exec($requete);
+        $connexion->exec('UPDATE croustapost SET ptsCrous = ptsCrous - 1 WHERE id = ' . $post_ID);
+    }
 
     session_start();
     header('Location: ' . $_SESSION['currentUrl']);
+}
+
+function dejaVote($croustagrameur_id, $croustapost_id) {
+// Test si un utilisateur a déja intéragie avec un post
+
+    global $connexion;
+
+    // Requête
+    $reqDejaVote = 'SELECT COUNT(*) FROM croustavote WHERE croustagrameur_id = \'' . $croustagrameur_id . '\' ' . 'AND croustapost_id = ' . $croustapost_id;
+    $boolDejaVote = $connexion->query($reqDejaVote);
+    $boolDejaVote = $boolDejaVote->fetch();
+
+    return $boolDejaVote[0];
 }
