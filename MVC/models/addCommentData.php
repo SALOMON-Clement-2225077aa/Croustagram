afd8d27c4b2595ddb3d1b80553edeb499a7517de
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2023-10-25 14:11:44 +0200

    Vérif de si les comms ajoutés sont vides ou non
    Signed-off-by: nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>

--- MVC/models/addCommentData.php
+++ MVC/models/addCommentData.php
@@ -9,14 +9,16 @@ $connexion = connexion();
 function addComment($contenu, $auteur, $idPost){
     global $today, $connexion;
 
-    $query = 'INSERT INTO croustacomm (texte, date, croustagrameur_id, croustapost_id) VALUES ("' .  $contenu . '", "' . $today . '", "' . $auteur . '", "' . $idPost . '")';
+    if($contenu!=='' and $contenu!==null and $contenu!==' '){
+        $query = 'INSERT INTO croustacomm (texte, date, croustagrameur_id, croustapost_id) VALUES ("' .  $contenu . '", "' . $today . '", "' . $auteur . '", "' . $idPost . '")';
 
-    if (!($dbResult = $connexion->exec($query))) {
-        echo '<strong>Erreur dans requête</strong><br>';
-        // Affiche le type d'erreur.
-        echo '<strong>Erreur : ' . $connexion->errorInfo() . '</strong><br>';
-        // Affiche la requête envoyée.
-        echo '<strong>Requête : ' . $query . '</strong><br>';
-        exit();
+        if (!($dbResult = $connexion->exec($query))) {
+            echo '<strong>Erreur dans requête</strong><br>';
+            // Affiche le type d'erreur.
+            echo '<strong>Erreur : ' . $connexion->errorInfo() . '</strong><br>';
+            // Affiche la requête envoyée.
+            echo '<strong>Requête : ' . $query . '</strong><br>';
+            exit();
+        }
     }
 }
