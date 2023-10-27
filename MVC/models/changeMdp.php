b06cca134666a812dcc6b3223f250196619f53ea
NathanGOUACHEG22017449 <nathan.gouache@etu.univ-amu.fr>
2023-10-27 14:39:33 +0200

    Fin de la rénit du mdp ?? Design CSS à faire...

---
+++ MVC/models/changeMdp.php
@@ -0,0 +1,30 @@
+<?php
+$mdp = htmlspecialchars($_POST['mdp']);
+$verifMdp = htmlspecialchars($_POST['verifMdp']);
+$accountName = $_POST['accountName'];
+
+require_once '../config/connectDatabase.php';
+
+$connexion = connexion();
+
+if ($mdp!==$verifMdp){
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+}
+else{
+    $query = 'UPDATE croustagrameur SET mdp=' . password_hash($mdp) .' WHERE id=' . $accountName;
+
+    if (!($dbResult = $connexion->exec($query))) {
+        echo '<strong>Erreur dans requête</strong><br>';
+        // Affiche le type d'erreur.
+        echo '<strong>Erreur : ' . $connexion->errorInfo() . '</strong><br>';
+        // Affiche la requête envoyée.
+        echo '<strong>Requête : ' . $query . '</strong><br>';
+        exit();
+    }
+    else{
+        echo '<label>Votre mot de passe a bien été changé !</label><br>
+<label>Vous serez redirigés dans 5 secondes...</label>';
+        sleep(5);
+        header('Location: ../views/viewMainPage.php');
+    }
+}
--- MVC/views/viewResetMdp.php
+++ MVC/views/viewResetMdp.php
@@ -2,7 +2,7 @@
 session_start();
 if (!isset($_GET['suid']) or !isset($_GET['accountId']) or ($_GET['suid'] !== $_SESSION['suid'])) header('Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
 
-echo '<form action="../controllers/resetMdp.php" method="post">
+echo '<form action="../models/changeMdp.php" method="post">
 <label>Entrez votre mot de passe</label>
 <input name="mdp" type="password"  placeholder="Mot de passe">
 <label>Vérifiez votre mot de passe</label>
