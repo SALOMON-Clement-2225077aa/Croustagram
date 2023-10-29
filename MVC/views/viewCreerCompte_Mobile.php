caf3c652ae4b1bd1422fa514df76bba2da070ceb
Clément Salomon <114286471+ClementRKG@users.noreply.github.com>
2023-10-29 17:10:23 +0100

    [Mobile] Fix mauvaise redirection quand on vote

--- MVC/views/viewCreerCompte_Mobile.php
+++ MVC/views/viewCreerCompte_Mobile.php
@@ -3,6 +3,7 @@
 require_once '../controllers/CroustagramGUI_Mobile.php';
 require_once '../controllers/controllerCreateAccount.php';
 
+$_SESSION['currentUrl'] = $_SERVER['REQUEST_URI'];
 if (isset($_SESSION['suid'])) header('Location :' . $_SESSION['currentUrl']);
 
 start_page('Crouscription');
