7c9c28e701c40e6dc64fda6275fdc00c67b5a87a
AlexandreBergeronb22003371 <alexandrebergeron12@gmail.com>
2023-10-29 19:51:19 +0100

    MDP OUBLIé MOBILE

--- MVC/views/viewResetMdp.php
+++ MVC/views/viewResetMdp.php
@@ -7,7 +7,10 @@ require '../controllers/CroustagramGUI_Mobile.php';
 
 $isMob = is_numeric(strpos(strtolower($_SERVER["HTTP_USER_AGENT"]), "mobile"));
 
-session_start();
+if (!$isMob) {
+    session_start();
+}
+
 if (!isset($_GET['suid']) or !isset($_GET['accountId']) or ($_GET['suid'] !== $_SESSION['suid'])) header('Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
 
 if ($isMob) {
