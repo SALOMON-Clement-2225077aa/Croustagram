d418c1e76acf8c68818656b844ac8c9ed1937e4a
Clément Salomon <114286471+ClementRKG@users.noreply.github.com>
2023-10-31 15:51:18 +0100

    [MOBILE] Affiche la position dans le classement

--- MVC/views/viewLeaderboard_Mobile.php
+++ MVC/views/viewLeaderboard_Mobile.php
@@ -5,7 +5,12 @@ require_once '../controllers/controllerLeaderboard.php';
 start_page('Leaderboard');
 
 echo '<div id="contenuClassement">';
+
+    if (isset($_SESSION['username'])) {
+        echo '<a class="User" id="position_Classement">Mon classement : ' . myPosition() . '</a>';
+    }
     showLeaderboard();
+
 echo '</div>';
 
 end_page();
